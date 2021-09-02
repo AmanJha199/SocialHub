0d8398efa8cec884acdb4ac5100c0ee42096c906
AmanJha199 <43825635+AmanJha199@users.noreply.github.com>
2021-09-03 01:11:26 +0530

    backend

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express();
 const PORT = 5000;
 const mongoose = require('mongoose');
 const {MONGOURI} = require('./keys');
-require('./models/user');
-//middleware to convert into json data
-app.use(express.json());
-//using auth.js as a middleware
-app.use(require('./routes/auth'));
+
 
 mongoose.connect(MONGOURI);
 
@@ -18,6 +14,16 @@ mongoose.connection.on('error', (err)=>{
     console.log("error in connecting MongoDB", err);
     
 })
+
+require('./models/user');
+require('./models/post');
+//middleware to convert into json data
+app.use(express.json());
+//using auth.js as a middleware
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'))
+
+
 //Listen to port 
 app.listen(PORT, ()=>{
     console.log("Server is running on port :" , PORT);
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {JWT_SECRET} = require('../keys')
-
+const { JWT_SECRET } = require('../keys')
+const requireLogin = require('../middleware/requireLogin')
 
 router.post('/signup', (req, res) => {
     //console.log(req.body);
@@ -20,12 +20,12 @@ router.post('/signup', (req, res) => {
             if (savedUser) {
                 return res.status(422).json({ error: "User already signed up" })
             }
-            bcrypt.hash(password, 14)
-                .then(hashpassword => {
+            bcrypt.hash(password, 12)
+                .then((hashpassword) => {
                     //if not created then we will create user ans then save it
                     const user = new User({
                         email,
-                        password,
+                        password : hashpassword,
                         name
                     })
                     user.save()
@@ -42,32 +42,39 @@ router.post('/signup', (req, res) => {
         })
 })
 
-router.post('/signin', (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(422).json({ error: "Add Email or password" })
+router.post('/signin',(req,res)=>{
+    
+    const {email,password} = req.body;
+    
+    if(!email || !password){
+       return res.status(422).json({error:"please add email or password"})
     }
-    User.findOne({ email: email })
-        .then(savedUser => {
-            if (!savedUser) {
-                return res.status(422).json({ error: "Invalid Email or password" })
+    User.findOne({email : email})
+    .then((savedUser) =>{
+        if(!savedUser){
+           return res.status(422).json({error:"Invalid Email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)   
+        .then((doMatch) => {
+            console.log(doMatch);
+            
+            if(doMatch){
+                //res.json({message:"successfully signed in"})
+                const token = jwt.sign({_id : savedUser._id}, JWT_SECRET)
+                res.json({token})
             }
-            bcrypt.compare(password, savedUser.password)
-                .then(doMatch => {
-                    if (doMatch) {
-                        //res.json({ message: "Successfully signed in" })
-                        const token = jwt.sign({_id : savedUser._id}, JWT_SECRET)
-                        res.json({token : token})
-                    }
-                    else {
-                        res.status(422).json({ error: "Invalid Email or password" })
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-
-                })
+            else{
+                return res.status(422).json({error:"Email or password not correct"})
+            }
         })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+        
+    })
 })
 
 module.exports = router;
